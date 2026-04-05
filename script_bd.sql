5fd80ba71241f14b4c35919b52abfdb4a0e28ba4
IsiOgas <isidoraogaspavez@gmail.com>
2026-04-04 23:32:44 -0400

    pequeña modificacion lol

--- script_bd.sql
+++ script_bd.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS ct_usm_postulaciones;
 CREATE DATABASE ct_usm_postulaciones;
 USE ct_usm_postulaciones;
 
@@ -35,22 +36,22 @@ CREATE TABLE Estado_Postulacion(
 
 -- DATOS A LAS TABLAS ??
 INSERT INTO Sede(ID_Sede, Nombre_Sede) VALUES
-(01, 'Casa Central Valparaíso'),
-(02, 'Campus Santiago San Joaquín'),
-(03, 'Campus Santiago Vitacura'),
-(04, 'Sede Viña del Mar'),
-(05, 'Sede Concepción');
+(1, 'Casa Central Valparaíso'),
+(2, 'Campus Santiago San Joaquín'),
+(3, 'Campus Santiago Vitacura'),
+(4, 'Sede Viña del Mar'),
+(5, 'Sede Concepción');
 
 INSERT INTO Region(ID_Region, Nombre_Region) VALUES
-(01, 'Arica y Parinacota'),
-(02, 'Tarapacá'),
-(03, 'Antofagasta'),
-(04, 'Atacama'),
-(05, 'Coquimbo'),
-(06, 'Valparaíso'),
-(07, 'Metropolitana'),
-(08, 'O''Higgins'),
-(09, 'Maule'),
+(1, 'Arica y Parinacota'),
+(2, 'Tarapacá'),
+(3, 'Antofagasta'),
+(4, 'Atacama'),
+(5, 'Coquimbo'),
+(6, 'Valparaíso'),
+(7, 'Metropolitana'),
+(8, 'O''Higgins'),
+(9, 'Maule'),
 (10, 'Ñuble'),
 (11, 'Biobío'),
 (12, 'La Araucanía'),
@@ -60,20 +61,20 @@ INSERT INTO Region(ID_Region, Nombre_Region) VALUES
 (16, 'Magallanes');
 
 INSERT INTO Tamanio_Empresa(ID_Tamanio, Nombre_Tamanio) VALUES
-(01, 'MicroEmpresa'),
-(02, 'Mediana'),
-(03, 'Grande');
+(1, 'MicroEmpresa'),
+(2, 'Mediana'),
+(3, 'Grande');
 
 INSERT INTO Tipo_Iniciativa(Id_Tipo_Iniciativa, Nombre_Tipo_Iniciativa) VALUES
-(01, 'Nueva'),
-(02, 'Existente');
+(1, 'Nueva'),
+(2, 'Existente');
 
 INSERT INTO Tipo_Persona(ID_Tipo_Persona, Nombre_Tipo_Persona) VALUES
-(01, 'Profesor'),
-(02, 'Estudiante');
+(1, 'Profesor'),
+(2, 'Estudiante');
 
 INSERT INTO Estado_Postulacion(ID_Estado, Nombre_Estado) VALUES
-(01, 'En Revisión'),
-(02, 'Aprobada'),
-(03, 'Rechazada'),
-(04, 'Cerrada');
+(1, 'En Revisión'),
+(2, 'Aprobada'),
+(3, 'Rechazada'),
+(4, 'Cerrada');
