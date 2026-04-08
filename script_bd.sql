5f64088976df6205481500080218ec2b4474bdea
aicr06 <acontrerasr@usm.cl>
2026-04-08 02:14:15 -0400

    casi lista

--- script_bd.sql
+++ script_bd.sql
@@ -241,7 +241,7 @@ VALUES
     ('6','2026-04-10','C06','19500000','Claudia Pinto','Héctor Salinas','76456789-0','4','2','3','1','1'),
     ('7','2026-04-15','D07','17500000','Luis Herrera','Carolina Vega','76234567-8','1','13','5','1','1'),
     ('8','2026-04-18','E08','21000000','Ricardo Núñez','Paula Torres','76345678-9','3','9','8','2','1'),
-    ('9','2026-04-20','F09','16000000','Claudia Pinto','Héctor Salinas','76456789-0','4','2','13','1','1'),
+    ('9','2026-04-20','F09','16000000','Claudia Pinto','Héctor Salinas','76456789-0','4','6','13','1','1'),
     ('10','2026-04-25','G10','25000000','Juan Pérez','María González','76123456-7','2','13','6','2','1');
 
 INSERT INTO Iniciativa(Documentos, Nombre_Iniciativa, Objetivo_Iniciativa, Descripcion_Soluciones, Resultados_Esperados,ID_Postulacion)
@@ -498,3 +498,79 @@ JOIN Tipo_Persona tp
 JOIN Sede s 
     ON ie.ID_Sede = s.ID_Sede
 WHERE et.Numero_Postulacion = 1;
+
+
+-- 5 Empresas con postulaciones y convenios
+SELECT
+    e.Nombre_Empresa AS 'Nombre',
+    ta.Nombre_tamanio AS 'Tamanio',
+    IF(e.Convenio_USM = 1, 'Sí', 'No') AS 'Convenio(si/no)',
+    COUNT(p.Rut_Empresa) AS 'Cantidad de postulaciones asociadas'
+FROM Entidad_Empresa e
+JOIN Tamanio_Empresa ta
+    ON e.ID_Tamanio = ta.ID_Tamanio
+JOIN Postulacion p
+    ON p.Rut_Empresa = e.Rut_Empresa
+GROUP BY e.Rut_Empresa, e.Nombre_Empresa, ta.Nombre_tamanio, e.Convenio_USM
+ORDER BY COUNT(p.Rut_Empresa) DESC;
+
+-- 6 Postulaciones con presupuesto sobre el promedio
+SELECT
+    p.Numero_Postulacion AS 'Numero postulacion',
+    e.Nombre_empresa AS 'Nombre empresa',
+    p.Presupuesto_Total AS 'Presupuesto total'
+FROM Postulacion p
+JOIN Entidad_Empresa e
+    ON p.Rut_Empresa = e.Rut_Empresa
+WHERE p.Presupuesto_Total> (SELECT AVG(Presupuesto_Total) FROM Postulacion)
+ORDER BY p.Presupuesto_Total DESC;
+
+-- 7 Cantidad de integrantes por postulacion y tipo  imprime una tabla feaa, en una linea 0 pof y cant alumnos y abajo cant prof y 0 alumnos:(
+SELECT 
+    et.Numero_Postulacion AS Postulacion, 
+    SUM(IF(tp.Nombre_Tipo_Persona = 'Profesor', 1, 0)) AS Profesores,
+    SUM(IF(tp.Nombre_Tipo_Persona = 'Estudiante', 1, 0)) AS Estudiantes
+FROM Equipo_de_Trabajo et 
+JOIN Integrante_Equipo ie 
+    ON et.ID_integrante = ie.ID_integrante
+JOIN Tipo_Persona tp 
+    ON ie.ID_Tipo_Persona = tp.ID_Tipo_Persona
+GROUP BY et.Numero_Postulacion, tp.Nombre_Tipo_Persona;
+
+-- 8 Postulaciones que no cumplen el minimo de equipo
+SELECT 
+    et.Numero_Postulacion AS Postulacion, 
+    SUM(IF(tp.Nombre_Tipo_Persona = 'Profesor', 1, 0)) AS Profesores,
+    SUM(IF(tp.Nombre_Tipo_Persona = 'Estudiante', 1, 0)) AS Estudiantes
+FROM Equipo_de_Trabajo et 
+JOIN Integrante_Equipo ie 
+    ON et.ID_integrante = ie.ID_integrante
+JOIN Tipo_Persona tp 
+    ON ie.ID_Tipo_Persona = tp.ID_Tipo_Persona
+GROUP BY et.Numero_Postulacion
+HAVING Profesores< 3 OR Estudiantes< 5;
+
+-- 9 Empresas sin postulaciones registradas
+SELECT
+    e.Nombre_Empresa AS 'Nombre empresa',
+    e.Rut_Empresa AS 'Rut_Empresa',
+    te.Nombre_Tamanio AS Tamanio
+FROM Entidad_Empresa e
+JOIN Tamanio_Empresa te 
+    ON te.ID_Tamanio= e.ID_Tamanio
+LEFT JOIN Postulacion p
+    ON p.Rut_Empresa = e.Rut_Empresa
+WHERE p.Rut_Empresa IS NULL;
+
+-- 10 Postulaciones que exceden el plazo maximo
+SELECT
+    p.Numero_Postulacion AS Postulacion, 
+    p.Codigo_Postulacion AS 'Codigo Postulacion',
+    COUNT(ec.ID_Etapa) AS 'Total de etapas',
+    SUM(ec.Plazos) AS 'Total semanas'
+FROM Postulacion p
+JOIN Etapa_Cronograma ec
+    ON ec.ID_Postulacion= p.Numero_Postulacion
+GROUP BY p.Numero_Postulacion
+HAVING SUM(ec.Plazos) > 36
+ORDER BY SUM(ec.Plazos) DESC;
